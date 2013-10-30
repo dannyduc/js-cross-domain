2c25ede7990977f830a067cc9ba72b83fe0f3c6b
Danny Nguyen <danny.duc@gmail.com>
2013-10-30 12:04:30 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+Run under tomcat 7
+URLs
+    * http://danny.ingenuity.com:8080/postMessage-proxy-for-xdr
+
+/etc/hosts
+127.0.0.1 danny.ingenuity.com
+127.0.0.1 api.ingenuity.dev
+
+#postMessage-proxy-for-xdr
+http://blogs.msdn.com/b/ieinternals/archive/2010/05/13/xdomainrequest-restrictions-limitations-and-workarounds.aspx
+http://www.webdbg.com/test/xdm/httptohttps.asp
+
+#cross-document-messaging-rpc
+http://msdn.microsoft.com/en-us/magazine/ff800814.aspx
+
---
+++ src/main/java/app/controller/AjaxController.java
@@ -0,0 +1,21 @@
+package app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+public class AjaxController {
+
+    @RequestMapping(value = "/doPost", method = RequestMethod.POST)
+    @ResponseBody
+    public Map<String, String> doPost() {
+        Map<String, String> m = new HashMap<String, String>();
+        m.put("message", "aok");
+        return m;
+    }
+}
